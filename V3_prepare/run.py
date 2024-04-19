3de6306ff513b393df0d48ce8252b52bfe87fcf9
v-zhazhai <v-zhazhai@microsoft.com>
2024-04-19 15:30:01 +0800

    updata 20240419 2

--- V3_prepare/run.py
+++ V3_prepare/run.py
@@ -1,27 +1,51 @@
 import os
+from step1_getaudioList import STEP1
+from step2_extractvttlist import step2
 from step3_extractvttlist import step3
 from step4_dump_json import step4
 from step5_UpdataBlob import step5
+import pandas as pd
 
 
-list_file_name = [r"FY23Q2\BingYouTube\Travel_Events",r"FY23Q2\YouTube\Travel_Events",r"FY23Q4-1\YouTube\Travel_Events",r"FY23Q4-2\YouTube\Travel_Events",r"FY23Q4-3\YouTube\Travel_Events",r"FY24Q1-1\YouTube\Travel_Events",r"FY24Q1-3\YouTube\Travel_Events",r"FY24Q2-1\YouTube\Travel_Events",r"FY24Q2-2\YouTube\Travel_Events",r"FY24Q2-3\YouTube\Travel_Events",r"FY24Q3-1\YouTube\Travel_Events"]
+# list_file_name = [r"FY23Q2\BingYouTube\Travel_Events",r"FY23Q2\YouTube\Travel_Events",r"FY23Q4-1\YouTube\Travel_Events",r"FY23Q4-2\YouTube\Travel_Events",r"FY23Q4-3\YouTube\Travel_Events",r"FY24Q1-1\YouTube\Travel_Events",r"FY24Q1-3\YouTube\Travel_Events",r"FY24Q2-1\YouTube\Travel_Events",r"FY24Q2-2\YouTube\Travel_Events",r"FY24Q2-3\YouTube\Travel_Events",r"FY24Q3-1\YouTube\Travel_Events"]
 
-locals = "fr-FR"
-inputdir = r"C:\Users\v-zhazhai\Downloads"
+locals = "pt-BR"
+inputdir = r"C:\Users\v-zhazhai\Desktop\TTS\pt-BR\YouTube"
 
-inputdirs = os.path.join(inputdir,locals)
+# inputdirs = os.path.join(inputdir,locals)
 
-
-batch = "batch15"
-
+step1 = STEP1()
+step2 = step2()
 step3 = step3()
 step4 = step4()
 step5 = step5()
 
-# print("step3_extractvttlist start")
-# step3.step3_extractvttlist(list_file_name,inputdirs,batch)
-# print("step4_dump_json start")
-# step4.step4_dump_json(list_file_name,inputdirs,batch)
+# print("step1_getaudioList start")
+# step1.run(inputdir,locals)
+# print("step2_extractvttlist start")
+# statistical_file = step2.run(inputdir,locals)
+statistical_file = r"C:\Users\v-zhazhai\Desktop\TTS\pt-BR\YouTube\pt-BR_statistical.csv"
+data = pd.read_csv(statistical_file,sep="\t",encoding='utf8',low_memory=False)
+path_list = [str(x.replace('\\audioList.txt','')) for x in data["path"]]
+domain_list = list(set([x for x in data["domain"]]))
 
-# step5.step5_updata_tts_filelist(locals,inputdir)
-step5.step5_updata_dump_json(locals,inputdir,int(batch.replace('batch','')))
+mapelist = []
+n = 1
+for line in domain_list:
+    batch = "batch"+str(n).zfill(2)
+    list_file_name = [x for i,x in enumerate(path_list) if x.find(line) != -1]
+    # print(line+'\t'+batch)
+    mapelist.append(line+'\t'+batch)
+
+    print("step3_extractvttlist {} start".format(line))
+    step3.step3_extractvttlist(list_file_name,inputdir,batch)
+    print("step4_dump_json {} start".format(line))
+    step4.step4_dump_json(list_file_name,inputdir,batch)
+
+    print("step5_updata_tts_filelist start")
+    step5.step5_updata_tts_filelist(locals,inputdir)
+    print("step5_updata_dump_json start")
+    step5.step5_updata_dump_json(locals,inputdir,int(batch.replace('batch','')))
+
+    n+=1
+print(mapelist)
---
+++ V3_prepare/step1_getaudioList.py
@@ -0,0 +1,64 @@
+import os
+
+class STEP1():
+    def __init__(self) -> None:
+        super().__init__()
+    # def init(self, snapshot_dir="", resource_dir_dict={}, arg_list=[]):
+        # self.inputdir = resource_dir_dict["--inputdir"]
+        # self.locals = resource_dir_dict["--locals"]
+    
+    def step1_getaudioList(self,inputdir,locals):
+        token = r'?sv=2023-01-03&ss=btqf&srt=sco&st=2024-04-19T02%3A51%3A37Z&se=2024-04-20T02%3A51%3A37Z&sp=rwdxftlacup&sig=ul1K0IUhN7psToYKH15JPKYcQEiSS6iiPb0oVChKZzY%3D'
+
+        output_list = os.path.join(inputdir,locals+'.txt')
+        word1 = 'C:/Users/v-zhazhai/Toosl/code/Tool/merger_tar/azcopy.exe list  "https://speechdatacrawlrgwusdiag.blob.core.windows.net/rawpublicdata/{}/{}" > "{}"'.format(locals,token,output_list)
+        print(word1)
+        os.system(word1)
+        return output_list
+    
+    def step1_download(self,output_list):
+        locals = os.path.split(output_list)[-1].replace('.txt','')
+        save_path =  '\\'.join(os.path.split(output_list)[:-1])
+
+        with open(output_list,'r',encoding='utf8') as f,open(output_list.replace(".txt","_v1.txt"),'w',encoding='utf8') as s:
+            for line in f.readlines()[2:]:
+                line = line.split(";")[0].replace("INFO: ",'')
+                if "audioList.txt" in line:
+
+                    token = r'?sv=2023-01-03&ss=btqf&srt=sco&st=2024-04-19T02%3A51%3A37Z&se=2024-04-20T02%3A51%3A37Z&sp=rwdxftlacup&sig=ul1K0IUhN7psToYKH15JPKYcQEiSS6iiPb0oVChKZzY%3D'
+
+                    audioList ='/'.join([locals,line])
+                    output_audioList = os.path.join(save_path,line.replace('/','\\'))
+
+                    word1 = 'C:/Users/v-zhazhai/Toosl/code/Tool/merger_tar/azcopy.exe copy  "https://speechdatacrawlrgwusdiag.blob.core.windows.net/rawpublicdata/{}{}" "{}"'.format(audioList,token,output_audioList)
+                    print(word1)
+                    os.system(word1)
+
+                    s.writelines(line+'\n')
+    
+    def run(self,inputdir,locals):
+        # output_list = r"C:\Users\v-zhazhai\Desktop\TTS\pt-BR\YouTube\pt-BR.txt"
+        output_list = self.step1_getaudioList(inputdir,locals)
+        self.step1_download(output_list)
+
+
+
+WAVE_INPUT_STEP = None
+
+def init():
+
+    global WAVE_INPUT_STEP
+    WAVE_INPUT_STEP = STEP1()
+
+    WAVE_INPUT_STEP.prs_step_init()
+
+def run(mini_batch):
+
+    return WAVE_INPUT_STEP.prs_step_run(mini_batch)
+
+if __name__ == "__main__":
+    inputdir = r"C:\Users\v-zhazhai\Desktop\TTS\pt-BR\YouTube"
+    locals =  "pt-BR"
+
+    step1 = STEP1()
+    step1.run(inputdir,locals)
