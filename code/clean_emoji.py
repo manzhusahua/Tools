0d4a8af5a4205af1f4a2c795bc8a10277db6c9de
v-zhazhai <v-zhazhai@microsoft.com>
2024-05-14 15:03:09 +0800

    updata 20240514 1

---
+++ code/clean_emoji.py
@@ -0,0 +1,15 @@
+import re
+def clean(desstr,restr=''):  
+    #过滤表情   
+    try:  
+        co = re.compile(u'['u'\U0001F300-\U0001F64F' u'\U0001F680-\U0001F6FF'u'\u2600-\u2B55]+')  
+    except re.error:  
+        co = re.compile(u'('u'\ud83c[\udf00-\udfff]|'u'\ud83d[\udc00-\ude4f\ude80-\udeff]|'u'[\u2600-\u2B55])+')  
+    return co.sub(restr, desstr)
+ 
+if __name__ == "__main__":
+    files_name = r"D:\users\v-zhazhai\TTS\zh-CN\20240229\output_chat_2\xlsx_all_split_30s\5.txt"
+    with open(files_name,'r',encoding='utf8') as f,open(files_name.replace(".txt","_clean.txt"),'w',encoding='utf8') as s:
+        for line in f.readlines():
+            line = clean(line)
+            s.writelines(line)
