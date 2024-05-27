b6c5b5ddbdbdf26350d4d8652fcf14cde5b8652f
v-zhazhai <v-zhazhai@microsoft.com>
2024-05-27 16:33:38 +0800

    updata 20240527 2

--- code/merger_files.py
+++ code/merger_files.py
@@ -1,10 +1,35 @@
-
 import os
+import codecs 
+import chardet
 
-files_path = r"C:\Users\v-zhazhai\debug\richland\F128\General\TextScripts"
+class MERGERDIR():
 
-for name in os.listdir(files_path):
-    with open(files_path+'.txt','a',encoding='utf8') as s:
-        with open(os.path.join(files_path,name),'r',encoding='utf16') as f:
-            for line in f.readlines():
-                s.writelines(line)
+    def __init__(self) -> None:
+        super().__init__()
+
+    def merger_files(self,inpudir,outputdir):
+        with open(outputdir,'w',encoding='utf8') as s:
+            for name in os.listdir(inpudir):
+                content=codecs.open(os.path.join(inpudir,name),'rb').read()
+                f = open(os.path.join(inpudir,name),'r',encoding=chardet.detect(content)['encoding']).read()
+                s.writelines(name.replace('.txt','')+'\t'+f+'\n') 
+
+INPUT_STEP = None
+
+def init():
+
+    global INPUT_STEP
+    INPUT_STEP = MERGERDIR()
+
+    INPUT_STEP.prs_step_init()
+
+def run(mini_batch):
+
+    return MERGERDIR.prs_step_run(mini_batch)
+
+if __name__ == "__main__":
+    merger_dir = MERGERDIR
+
+    inpudir = r""
+    outputdir = r""
+    merger_dir.merger_files()
