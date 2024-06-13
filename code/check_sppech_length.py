d77e61c7b7cfd17ce48f97ced714697d2e09b15b
Zhaohui Zhai (Shanghai Wicresoft Co Ltd) <v-zhazhai@microsoft.com>
2024-06-13 14:21:16 +0800

    updata 20240613 1

--- code/check_sppech_length.py
+++ code/check_sppech_length.py
@@ -1,12 +1,17 @@
 import wave
 import os
-wav_path = r"C:\Users\v-zhazhai\Downloads\111"
+import glob
 
-time_counts=[]
-for name in os.listdir(wav_path):
-    wav_name = os.path.join(wav_path,name)
-    f = wave.open(wav_name, 'rb')
-    time_count = f.getparams().nframes/f.getparams().framerate
-    time_counts.append(time_count)
-print(sum(time_counts))
-print(str(round(sum(time_counts)/3600, 5)))
+def get_wav_time(wav_path):
+    time_counts=[]
+    for file in glob.glob(os.path.join(wav_path,"**\\*.*"), recursive=True):
+        if ".wav" in file:
+            f = wave.open(file, 'rb')
+            time_count = f.getparams().nframes/f.getparams().framerate
+            time_counts.append(time_count)
+    print(sum(time_counts))
+    print(str(round(sum(time_counts)/3600, 5)))
+
+if __name__ == "__main__":
+    wav_path = r"D:\users\v-zhazhai\TTS\zh-CN\M400\20240508\Wave48kNormalized2"
+    get_wav_time(wav_path)
