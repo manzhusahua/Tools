7ec0ca761d5d48ffa5f55ee1abcf7f3563dc7d71
v-zhazhai <v-zhazhai@microsoft.com>
2024-08-13 16:20:20 +0800

    updata 20240813 3

--- prepare/TTS_checkin.py
+++ prepare/TTS_checkin.py
@@ -1,26 +1,31 @@
 import os
+
 """
 主要用于生成checkin所需要的 Alignment、Speech、TextScripts、XmlScripts文件夹
 """
-class TTSCheckin():
+
+
+class TTSCheckin:
     def __init__(self) -> None:
         super().__init__()
-    
-    names = ["Alignment","Speech","TextScripts","XmlScripts"]
-    def wave_floder(inputdir,wave_list):
-        for name in wave_list:
-            for 
+
+    names = ["Alignment", "Speech", "TextScripts", "XmlScripts"]
+
+    def wave_floder(self, inputdir, wave_list):
+        for wave_name in wave_list:
+            for name in self.names:
+                save_path = os.path.join(inputdir, wave_name, name)
+                if not os.path.exists(save_path):
+                    os.makedirs(save_path, exist_ok=True)
+
+    def prepare_folder(self, inputdir):
+        for name in self.names:
+            save_path = os.path.join(inputdir, name)
             if not os.path.exists(save_path):
                 os.makedirs(save_path, exist_ok=True)
 
-    def prepare_folder(inputdir):
-        names = ["Alignment","Speech","TextScripts","XmlScripts"]
-        for name in names:
-            save_path = os.path.join(inputdir,name)
-            if not os.path.exists(save_path):
-                os.makedirs(save_path, exist_ok=True)
 
-if __name__=="__main__":
+if __name__ == "__main__":
     input_dir = r"D:\users\v-zhazhai\Tools\TTSData\ttsdata\ttsdata\zh-CN\Voices\Others\Honor\CNV"
     for name in os.listdir(input_dir):
-        prepare_folder(os.path.join(input_dir,name))
+        prepare_folder(os.path.join(input_dir, name))
