4ae463140b6ff1b3ce98eff94da8a0c362f5e572
v-zhazhai <v-zhazhai@microsoft.com>
2024-10-31 11:19:28 +0800

    updata 20241031 1

--- code/clean_bloblist.py
+++ code/clean_bloblist.py
@@ -1,30 +1,49 @@
 import os
 import sys
+
+
 # files = sys.argv[1]
 def clean(files):
     # files = r"C:\Users\v-zhazhai\Desktop\en-US_short_form_fix_r8_filenames.txt"
-    with open(files,'r',encoding='utf8') as f,open(files.replace(".txt","_v1.txt"),'w',encoding='utf8') as s:
+    with open(files, "r", encoding="utf8") as f, open(
+        files.replace(".txt", "_v1.txt"), "w", encoding="utf8"
+    ) as s:
         chunk_ids = []
         chunk_id = []
         for line in f.readlines()[2:]:
-            lines = line.split(";")[0].replace("INFO: ",'')
+            lines = line.split(";")[0].replace("INFO: ", "")
             # if ".info\n" in line and ".json.version_" not in line:
             if ".info" in lines:
-                s.writelines(lines+'\n')
-                
+                s.writelines(lines + "\n")
+
                 # chunk_ids.append(line)
             # line_id = line.split(".")[0]
             # if line_id not in chunk_id :
         #     #     chunk_id.append(line_id)
         # for id in chunk_id:
         #     if len([i for i,x in enumerate(chunk_ids) if x.find(id)!=-1]) == 7:
-            # s.writelines(line+'\n')
+        # s.writelines(line+'\n')
+
 
 def replace(files):
-    word = list(set(open(files,'r',encoding='utf8').readlines()))
-    with open(files.replace('.txt',"_clean.txt"),'w',encoding='utf8') as s:
+    word = list(set(open(files, "r", encoding="utf8").readlines()))
+    with open(files.replace(".txt", "_clean.txt"), "w", encoding="utf8") as s:
         for line in word:
             s.writelines(line)
 
+
+def clean_job_list(input, output):
+    with open(input, "r", encoding="utf8") as f, open(
+        output, "w", encoding="utf8"
+    ) as s:
+        for line in f.readlines():
+            if "\t" in line:
+                s.writelines(line)
+    os.remove(input)
+
+
 if __name__ == "__main__":
-    clean(r"C:\Users\v-zhazhai\Desktop\en-US_short_form_fix_r8_filenames.txt")
+    clean_job_list(
+        r"C:\Users\v-zhazhai\Downloads\filenames.txt",
+        r"C:\Users\v-zhazhai\Downloads\batch23_part7.txt",
+    )
---
+++ code/files_data_process/zipDir.py
@@ -0,0 +1,47 @@
+import zipfile
+import os, sys
+import shutil
+
+
+class ZIPDIR:
+    def __init__(self) -> None:
+        super().__init__()
+
+    def zipDir(self, dirpath, outFullName):
+        """
+        压缩指定文件夹
+        :param dirpath: 目标文件夹路径
+        :param outFullName: 压缩文件保存路径+xxxx.zip
+        :return: 无
+        """
+        zip = zipfile.ZipFile(outFullName, "w", zipfile.ZIP_DEFLATED)
+        for path, dirnames, filenames in os.walk(dirpath):
+            # 去掉目标跟路径，只对目标文件夹下边的文件及文件夹进行压缩
+            fpath = path.replace(dirpath, "")
+
+            for filename in filenames:
+                print(filename)
+                zip.write(os.path.join(path, filename), os.path.join(fpath, filename))
+        zip.close()
+
+        # shutil.rmtree(dirpath, ignore_errors=True)
+
+
+INPUT_STEP = None
+
+
+def init():
+
+    global INPUT_STEP
+    INPUT_STEP = ZIPDIR()
+
+    INPUT_STEP.prs_step_init()
+
+
+def run(mini_batch):
+
+    return INPUT_STEP.prs_step_run(mini_batch)
+
+
+if __name__ == "__main__":
+    zip_Dir = ZIPDIR()
