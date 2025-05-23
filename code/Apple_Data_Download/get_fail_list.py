3ac5215d4aa1c76c5ee1ba6b035e7465e8d42e64
v-zhazhai <v-zhazhai@microsoft.com>
2025-05-23 10:46:13 +0800

    20250523 updata1

--- code/Apple_Data_Download/get_fail_list.py
+++ code/Apple_Data_Download/get_fail_list.py
@@ -2,7 +2,7 @@
 用于获取fasttranscription 失败的filelist
 """
 
-import os
+import os,sys
 
 def get_fail_list(logdir,savelist):
     with open(savelist, 'w') as s:
@@ -11,8 +11,19 @@ def get_fail_list(logdir,savelist):
                 for line in f.readlines():
                     if "get sr result failed" in line:
                         s.writelines(line.split('"')[1]+'\n')
+
+def get_fail_lists(jsondir,filelist):
+    jsonlist = open(jsondir,'r').readlines()
+    with open(filelist, 'r') as f,open(os.path.join(os.path.split(filelist)[0],"fail_list.txt"),'w') as s:
+        for line in f.readlines():
+            names = "_".join(line.split('/')[-2:]).replace(".mp3",".json")
+            if names not in jsonlist:
+                s.writelines(line)
+            
+
 if __name__ == "__main__":
     logdir = r"C:\Users\v-zhazhai\Downloads\log"
-    savelist = r'C:\Users\v-zhazhai\Downloads\frca.txt'
+    savelist = r"C:\Users\v-zhazhai\Downloads\frfr_clean.txt"
+    # get_fail_lists(sys.argv[1],sys.argv[2])
     get_fail_list(logdir,savelist)
     
